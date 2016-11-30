f4d2be6021b5b19fc68b256c2d54d11532e5eff0
Tyler Springer <mspringer@smu.edu>
2016-11-29 17:52:46 -0800

    Re-wrote Java implementation to use iterators instead of directly indexing into the ArrayLists

--- java/MergeSort.java
+++ java/MergeSort.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class MergeSort
 {
@@ -10,8 +11,10 @@ public class MergeSort
 
 		public ArrayList<Integer> divide(final ArrayList<Integer> vals, final int start, final int end){
 			if(start > end)
+			{
 				return null;
-			if(start == end)
+			}
+			else if(start == end)
 			{
 				ArrayList<Integer> v = new ArrayList<Integer>(1);
 				v.add(vals.get(start));
@@ -25,34 +28,35 @@ public class MergeSort
 
 		public ArrayList<Integer> merge(final ArrayList<Integer> list1, final ArrayList<Integer> list2)
 		{
-			int i, j;
-			i = 0;
-			j = 0;
 			ArrayList<Integer> ret = new ArrayList<Integer>(list1.size() + list2.size());
-			while((i < list1.size()) && (j < list2.size()))
+			Iterator<int> list1_it = list1.iterator();
+			Iterator<int> list2_it = list2.iterator();
+			int val1 = list1_it.next();
+			int val2 = list2_it.next();
+			while(list1_it.hasNext() && list2_it.hasNext())
 			{
-				if(list1.get(i) <= list2.get(j))
+				if(val1 <= val2)
 				{
-					ret.add(list1.get(i));
-					i++;
+					ret.add(val1);
+					val1 = list1_it.next();
 				}
 				else
 				{
-					ret.add(list2.get(j));
-					j++;
+					ret.add(val2);
+					val2 = list2_it.next();
 				}
 			}
 
-			while(i < list1.size())
+			while(list1_it.hasNext())
 			{
-				ret.add(list1.get(i));
-				i++;
+				ret.add(val1);
+				val1 = list1_it.next();
 			}
 
-			while(j < list2.size())
+			while(list2_it.hasNext())
 			{
-				ret.add(list2.get(j));
-				j++;
+				ret.add(val2);
+				val2 = list2_it.next();
 			}
 			return ret;
 		}
